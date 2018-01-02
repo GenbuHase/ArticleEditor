177e6cc13df46102f83e4fd1eeffb14311a41345
Genbu Hase <hasegenbu@gmail.com>
2018-01-02 15:33:03 +0900

    fixup! v1.0.0

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 "use strict";
 const express = require("express");
+const setting = require("./setting");
 
 
 
 let app = express();
-	app.listen(process.env.PORT, () => {
-		console.log(`[Article Editor] I'm running on port ${process.env.PORT}!!`);
+	app.listen(setting.PORT, () => {
+		console.log(`[Article Editor] I'm running on port ${setting.PORT}!!`);
 	});
 
 	app.get(/(.*)/, (req, res) => res.sendFile(`${__dirname}/${req.url.replace(/%20/g, " ")}`));
