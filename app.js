8f5101319bbe845c9b80ac99db7b06cd1f208612
Genbu Hase <hasegenbu@gmail.com>
2018-02-11 19:35:09 +0900

    取得などのapiを追加

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const bodyParser = require("body-parser");
 const setting = require("./setting");
 
@@ -6,15 +7,32 @@ const setting = require("./setting");
 
 let app = express();
 	app.use(bodyParser.json());
-	
-	app.listen(setting.PORT, () => {
-		console.log(`[Article Editor] I'm running on port ${setting.PORT}!!`);
+
+	app.get("/api/articles", (req, res) => {
+		res.end(JSON.stringify({
+			amount: fs.readdirSync("articles").length
+		}));
+	});
+
+	app.post("/api/article", (req, res) => {
+		let { title, createdAt, content } = req.body;
+
+		let mixedContent = "";
+
+		try {
+			mixedContent = fs.readFileSync("template/index.html", "UTF-8").replace(/\${title}/g, title).replace(/\${createdAt}/g, createdAt).replace(/\${content}/g, content);
+			console.log(mixedContent);
+		} catch (err) {
+			console.error("Error: テンプレートHTML(template/index.html)が存在しません");
+
+			throw err;
+		}
+
+		res.end(mixedContent);
 	});
 
 	app.get(/.*/, (req, res) => res.sendFile(`${__dirname}/${req.url.replace(/%20/g, " ")}`));
 
-	app.post("/save", (req, res) => {
-		console.log(req.body);
-
-		res.end();
+	app.listen(setting.PORT, () => {
+		console.log(`[Article Editor] I'm running on port ${setting.PORT}!!`);
 	});
--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
-	document.querySelectorAll('.datepicker').forEach(picker => {
+	const articleNumber = document.querySelector("#Editor-Info-ArticleNumber");
+	const articleTitle = document.querySelector("#Editor-Info-Title");
+	const articleCreatedAt = document.querySelector("#Editor-Info-CreatedAt");
+	const articleContent = document.querySelector("#Editor-Content");
+
+	document.querySelectorAll("Select").forEach(selectBox => M.Select.init(selectBox));
+
+	document.querySelectorAll(".datepicker").forEach(picker => {
+		picker.value = `${new Date().getFullYear()}/${new Date().getMonth() + 1}/${new Date().getDate()}`,
+		picker.parentElement.querySelector("Label").classList.add("active");
+
 		M.Datepicker.init(picker, {
 			format: "yyyy/mm/dd",
 			defaultDate: new Date()
@@ -9,11 +19,34 @@ window.addEventListener("DOMContentLoaded", () => {
 	document.querySelector("#Editor-Btns-Save").addEventListener("click", () => {
 		DOM.xhr({
 			type: "POST",
-			url: "/save",
+			url: "/api/article",
+			doesSync: true,
+
+			headers: {
+				"Content-Type": "application/json"
+			},
 
 			data: JSON.stringify({
-				createdAt: document.querySelector("#Editor-Info-CreatedAt").value
-			}, null, "\t")
+				title: articleTitle.value,
+				createdAt: articleCreatedAt.value,
+
+				content: articleContent.value
+			})
 		});
+	});
+
+	DOM.xhr({
+		type: "GET",
+		url: "/api/articles",
+		doesSync: true,
+		resType: "json",
+
+		onLoad (event) {
+			for (let i = 0; i < event.target.response.amount; i++) {
+				articleNumber.querySelector("Optgroup:Last-Child").appendChild(new Option(i + 1, i + 1));
+			}
+
+			M.Select.init(articleNumber);
+		}
 	});
 });
