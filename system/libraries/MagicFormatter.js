058ee60b89d41413269b736020bc06e607e1de73
Genbu Hase <hasegenbu@gmail.com>
2018-03-29 13:00:13 +0900

    hintAnchorの改良

--- system/libraries/MagicFormatter.js
+++ system/libraries/MagicFormatter.js
@@ -7,13 +7,13 @@ module.exports = class MagicFormatter {
 	}
 
 	get forPreview () {
-		let formatted = this.hint.commonImageForPreview.articleImageForPreview.hintAnchor.blankAnchor.anchor.emphasis.italic.strikeThrough;
+		let formatted = this.hint().commonImageForPreview().articleImageForPreview().hintAnchor().blankAnchor().anchor().emphasis().italic().strikeThrough();
 
 		return formatted.content;
 	}
 
 	get forPublish () {
-		let formatted = this.hint.commonImageForPublish.articleImageForPublish.hintAnchor.blankAnchor.anchor.emphasis.italic.strikeThrough;
+		let formatted = this.hint().commonImageForPublish().articleImageForPublish().hintAnchor().blankAnchor().anchor().emphasis().italic().strikeThrough();
 
 		return formatted.content;
 	}
@@ -21,51 +21,77 @@ module.exports = class MagicFormatter {
 
 
 	//*:強調文字列*
-	get emphasis () {
+	emphasis () {
 		return new MagicFormatter(this.id, this.content.replace(/\*((?:\S|[ \u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff](?!\*))+)\*(?!(?:\S))/g, '<B>$1</B>'));
 	}
 
 	//_:斜体文字列_
-	get italic () {
+	italic () {
 		return new MagicFormatter(this.id, this.content.replace(/_((?:\S|[ \u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff](?!_))+)_(?!(?:\S))/g, '<I>$1</I>'));
 	}
 
 	//-:打ち消し文字列-
-	get strikeThrough () {
+	strikeThrough () {
 		return new MagicFormatter(this.id, this.content.replace(/-((?:\S|[ \u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff](?!-))+)-(?!(?:\S))/g, '<S>$1</S>'));
 	}
 
 	//[:アンカー文字列](:リンク先URL)
-	get anchor () {
+	anchor () {
 		return new MagicFormatter(this.id, this.content.replace(/\[([^\]]*)\]\(([^\)]+)\)/g, '<A Href = "$2">$1</A>'));
 	}
 	
 	//:[:アンカー文字列](:リンク先URL)
-	get blankAnchor () {
+	blankAnchor () {
 		return new MagicFormatter(this.id, this.content.replace(/:\[([^\]]*)\]\(([^\)]+)\)/g, '<A Href = "$2" Target = "_blank">$1</A>'));
 	}
 
 	//![:Alt属性](:画像URL)
-	get articleImageForPreview () {
+	articleImageForPreview () {
 		return new MagicFormatter(this.id, this.content.replace(/!\[([^\]]*)\]\(([^\)]+)\)/g, `<Img Src = "/${CONFIG.PATH.MEDIA}/${this.id}/$2" Alt = "$1" />`));
 	}
 
-	get articleImageForPublish () {
+	articleImageForPublish () {
 		return new MagicFormatter(this.id, this.content.replace(/!\[([^\]]*)\]\(([^\)]+)\)/g, '<Img Src = "$2" Alt = "$1" />'));
 	}
 
 	//!^[:Alt属性](:画像URL)
-	get commonImageForPreview () {
+	commonImageForPreview () {
 		return new MagicFormatter(this.id, this.content.replace(/!\^\[([^\]]*)\]\(([^\)]+)\)/g, `<Img Src = "/${CONFIG.PATH.COMMONMEDIA}/$2" Alt = "$1" />`));
 	}
 
-	get commonImageForPublish () {
+	commonImageForPublish () {
 		return new MagicFormatter(this.id, this.content.replace(/!\^\[([^\]]*)\]\(([^\)]+)\)/g, '<Img Src = "../common/$2" Alt = "$1" />'));
 	}
 
-	//@[:注釈アンカー文字列](:注釈ラベル名)
-	get hintAnchor () {
-		return new MagicFormatter(this.id, this.content.replace(/@\[(.+)\]\((.+)\)/g, '$1<A Href = "#$2"><Sup>[$2]</Sup></A>'));
+	//@[:注釈アンカー文字列](:注釈ラベル1)(:注釈ラベル2)...(:注釈ラベルn)
+	hintAnchor () {
+		//注釈あり<A Href = "#ABC"><Sup>[ABC]</Sup></A>~
+		let formatted = this.content;
+
+		const MATCHER_HINT_ANCHOR = /@\[(\S+)\]\((\S+)\)/g;
+		let hintAnchors = [],
+			mHintAnchor;
+			
+		while ((mHintAnchor = MATCHER_HINT_ANCHOR.exec(this.content)) != null) {
+			hintAnchors.push(mHintAnchor);
+		}
+		
+		const MATCHER_HINT = /\|?([^|]+)/y;
+		for (let i in hintAnchors) {
+			let hintAnchor = hintAnchors[i],
+				formattedHintAnchor = hintAnchor[1],
+				hints = hintAnchor[2];
+
+			let mHint;
+
+			while ((mHint = MATCHER_HINT.exec(hints)) != null) {
+				formattedHintAnchor += `<A Href = "#${mHint[1]}"><Sup>[${mHint[1]}]</Sup></A>`;
+			}
+			
+			formatted = formatted.replace(hintAnchor[0], formattedHintAnchor);
+		}
+
+		return new MagicFormatter(this.id, formatted);
 	}
 
 	/*
@@ -73,7 +99,7 @@ module.exports = class MagicFormatter {
 	 * :内容
 	 * @@
 	 */
-	get hint () {
-		return new MagicFormatter(this.id, this.content.replace(/@([^\[\](?:\r?\n)]+)\r?\n([^]+)\r?\n@@/g, [ '<Span ID = "$1">[$1]</Span>', '$2' ].join("\n")));
+	hint () {
+		return new MagicFormatter(this.id, this.content.replace(/^@([^[\]\r\n]+)\r?\n((?:.|\r?\n(?!@@))*)\r?\n@@$/gm, [ '<Span ID = "$1">[$1]</Span>', '$2' ].join("\n")));
 	}
 }
